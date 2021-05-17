40739978b68ae7f7e475d12ee889daa54d588891
GabrielL <79658132+gluna1203@users.noreply.github.com>
2021-05-17 09:12:30 -0600

    List PT

---
+++ List/Main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "List.h"
+int main()
+{
+
+	//nc::list<int> lst;
+	//lst.push_front(1);
+	//lst.push_front(2);
+	//lst.push_front(3);
+
+	//lst.write(std::cout) << std::endl;
+	//std::cout << "list\n";
+	nc::list<int> numbers{ 12, 78, 12, 34, 9 };
+	numbers.push_back(10);
+	numbers.push_front(8);
+	numbers.write(std::cout);
+
+	std::cout << "copy\n";
+	nc::list<int> copy;
+	copy = numbers;
+	copy.pop_front();
+	copy.pop_back();
+	copy.pop_back();
+	copy.write(std::cout);
+
+	std::cout << "remove\n";
+	copy.remove(12);
+	copy.write(std::cout);
+
+	std::cout << "resize\n";
+	copy.resize(10, 99);
+	copy.write(std::cout);
+
+	std::cout << "reverse\n";
+	numbers.reverse();
+	numbers.write(std::cout);
+
+	std::cout << "front: " << numbers.front() << std::endl;
+	std::cout << "back: " << numbers.back() << std::endl;
+	std::cout << "size: " << numbers.size() << std::endl;
+	std::cout << "empty: " << numbers.empty() << std::endl;
+}
